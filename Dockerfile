2d38e084cdda9550de4ab368ab733e55325c2bd6
Martin Halamíček <martin@halamicek.cz>
2016-04-16 17:13:09 +0200

    init

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM php:7.0.5-apache
+MAINTAINER Martin Halamicek <martin@keboola.com>
+
+ADD . /var/www/html
+
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinhalamicek
+ * Date: 16/04/16
+ * Time: 16:53
+ */
+
+header('content-type: application/json');
+
+echo json_encode([
+    'status' => 'ok',
+]);
